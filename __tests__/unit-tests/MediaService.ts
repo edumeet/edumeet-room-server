c454baa5e0f657afd18c5f92d0a474205a5b7256
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2022-12-05 11:52:28 +0100

    test: add unit-test for mediaservice class

--- __tests__/unit-tests/MediaService.ts
+++ __tests__/unit-tests/MediaService.ts
@@ -30,16 +30,22 @@ describe('MediaService', () => {
 	describe('Router', () => {
 		const ERROR_MSG_NO_MEDIA_NODES = 'no media nodes available';
 		const ERROR_MSG_ROOM_CLOSED = 'room closed';
-		let fakeMediaNode: MediaNode;
+		let fakeMediaNode1: MediaNode;
+		let fakeMediaNode2: MediaNode;
+		let mockGetRouter1: jest.SpyInstance;
+		let mockGetRouter2: jest.SpyInstance;
+		let spyMediaNode1GetRouter: jest.SpyInstance;
+		let spyMediaNode2GetRouter: jest.SpyInstance;
 		let fakeRoom: Room;
 		let fakePeer: Peer;
 		let spyRoomAddRouter: jest.SpyInstance;
-		let mockGetRouter: jest.SpyInstance;
 
 		beforeEach(() => {
-			mockGetRouter = jest.fn().mockImplementation(async () => {	
-				return { id: 'id',
-					close: jest.fn() } as unknown as Router;
+			mockGetRouter1 = jest.fn().mockImplementation(async () => {	
+				return { close: jest.fn() } as unknown as Router;
+			});
+			mockGetRouter2 = jest.fn().mockImplementation(async () => {	
+				return { close: jest.fn() } as unknown as Router;
 			});
 			fakeRoom = {
 				id: 'id',
@@ -49,14 +55,19 @@ describe('MediaService', () => {
 			fakePeer = {
 				id: 'id'
 			} as unknown as Peer;
-			fakeMediaNode = {
-				getRouter: mockGetRouter
+			fakeMediaNode1 = {
+				getRouter: mockGetRouter1
+			} as unknown as MediaNode;
+			fakeMediaNode2 = {
+				getRouter: mockGetRouter2
 			} as unknown as MediaNode;
 			spyRoomAddRouter = jest.spyOn(fakeRoom, 'addRouter');
+			spyMediaNode1GetRouter = jest.spyOn(fakeMediaNode1, 'getRouter');
+			spyMediaNode2GetRouter = jest.spyOn(fakeMediaNode2, 'getRouter');
 		});
 
 		it('getRouter() - Should add router to room when parent not closed', async () => {
-			mediaService.mediaNodes.add(fakeMediaNode);
+			mediaService.mediaNodes.add(fakeMediaNode1);
 			expect(mediaService.mediaNodes.length).toBe(1);
 			
 			await mediaService.getRouter(fakeRoom, fakePeer);
@@ -67,11 +78,22 @@ describe('MediaService', () => {
 		it('getRouter() - Should throw when parent room have closed', async () => {
 			const roomWithClosedParent = { ...fakeRoom, parentClosed: true } as unknown as Room;
 
-			mediaService.mediaNodes.add(fakeMediaNode);
+			mediaService.mediaNodes.add(fakeMediaNode1);
 			
 			await expect(mediaService.getRouter(roomWithClosedParent, fakePeer)).
 				rejects.toThrowError(ERROR_MSG_ROOM_CLOSED);
 			expect(spyRoomAddRouter).not.toHaveBeenCalled();
+		});
+		
+		it('getRouter() - Should spread routers across mediaNodes', async () => {
+			mediaService.mediaNodes.add(fakeMediaNode1);
+			mediaService.mediaNodes.add(fakeMediaNode2);
+			
+			mediaService.getRouter(fakeRoom, fakePeer);
+			mediaService.getRouter(fakeRoom, fakePeer);
+
+			expect(spyMediaNode1GetRouter).toHaveBeenCalledTimes(1);
+			expect(spyMediaNode2GetRouter).toHaveBeenCalledTimes(1);
 		});
 		
 		// This needs to be last since index is set to NaN
