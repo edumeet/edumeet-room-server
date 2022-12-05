64002513bb1e5da9c83c152ea0e59cf1805466de
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2022-12-05 09:24:19 +0100

    test: add unit-tests for peer class

--- __tests__/unit-tests/Peer.ts
+++ __tests__/unit-tests/Peer.ts
@@ -1,13 +1,33 @@
-import { BaseConnection, IOServerConnection } from 'edumeet-common';
+const mockLoggerDebug = jest.fn();
+const mockLoggerError= jest.fn();
+const mockLoggerWarn= jest.fn();
+
+jest.mock('edumeet-common', () => {
+	const originalModule = jest.requireActual('edumeet-common');
+	
+	return {
+		...originalModule,
+		Logger: jest.fn().mockImplementation(() => {
+			return {
+				debug: mockLoggerDebug,
+				error: mockLoggerError,
+				warn: mockLoggerWarn
+			};
+		})
+	}; 
+});
+
+import { BaseConnection, IOServerConnection, SocketMessage } from 'edumeet-common';
 import 'jest';
 import { Socket } from 'socket.io';
 import { userRoles } from '../../src/common/authorization';
-import { Peer } from '../../src/Peer';
+import { Peer, PeerContext } from '../../src/Peer';
 import { Producer } from '../../src/media/Producer';
 import { Consumer } from '../../src/media/Consumer';
 import { WebRtcTransport } from '../../src/media/WebRtcTransport';
 import { Router } from '../../src/media/Router';
 import EventEmitter from 'events';
+import { Pipeline } from 'edumeet-common';
 
 describe('Peer', () => {
 	let connection: BaseConnection;
@@ -161,28 +181,7 @@ describe('Peer', () => {
 		expect(spyEmit).toHaveBeenCalledTimes(0);
 	});
 
-	it('addConnection()', () => {
-		const connection2 = new IOServerConnection(mockSocket);
-
-		expect(peer.connections.items.length).toBe(1);
-
-		peer.addConnection(connection2);
-
-		expect(peer.connections.items.length).toBe(2);
-	});
-
-	it('addConnection() - handle close', () => {
-		const spyClose = jest.spyOn(peer, 'close');
-
-		expect(peer.connections.length).toBe(1);
-
-		connection.close();
-
-		expect(peer.connections.length).toBe(0);
-		expect(spyClose).toHaveBeenCalled();
-	});
-
-	describe('router', () => {
+	describe('Router', () => {
 		let router: Router;
 
 		beforeEach(() => {
@@ -213,6 +212,131 @@ describe('Peer', () => {
 			emitRouter.emit('close');
 		
 			expect(peer.router).toBe(undefined);
+		});
+	});
+
+	describe('Connections', () => {
+		let DEBUG_MSG_ADD_CONNECTION: string;
+		let REJECT_MESSAGE_SERVER_ERROR: string;
+		let pipelineWithMiddleware: Pipeline<PeerContext>;
+		let pipelineWithoutMiddleware: Pipeline<PeerContext>;
+		let mockRequest: jest.SpyInstance; 
+		let mockRespond: jest.SpyInstance;  
+		let mockReject: jest.SpyInstance;  
+		let spyPipelineExecute: jest.SpyInstance;
+		let spyRequest: jest.SpyInstance;
+		let mockSocketMessage: SocketMessage;
+
+		beforeAll(() => {
+			DEBUG_MSG_ADD_CONNECTION = 'addConnection()';
+			REJECT_MESSAGE_SERVER_ERROR = 'Server error';
+		});
+		
+		beforeEach(() => {
+			mockSocketMessage = {
+			} as unknown as SocketMessage;
+			pipelineWithoutMiddleware = {
+				execute: (context: PeerContext) => {
+					context.handled = false; 
+				}
+			} as unknown as Pipeline<PeerContext>;
+			pipelineWithMiddleware = {
+				execute: (context: PeerContext) => {
+					context.handled = true; 
+				}
+			} as unknown as Pipeline<PeerContext>;
+			mockRequest = jest.fn();
+			mockRespond = jest.fn();
+			mockReject = jest.fn();
+			spyPipelineExecute = jest.spyOn(peer.pipeline, 'execute');
+			spyRequest = jest.spyOn(connection, 'request');
+		});
+
+		afterEach(() => {
+			jest.clearAllMocks();
+		});
+
+		it('addConnection() - should increment connection count', () => {
+			const connection2 = new IOServerConnection(mockSocket);
+
+			expect(peer.connections.items.length).toBe(1);
+
+			peer.addConnection(connection2);
+
+			expect(peer.connections.items.length).toBe(2);
+		});
+
+		it('addConnection() - handle close', () => {
+			const spyClose = jest.spyOn(peer, 'close');
+
+			expect(peer.connections.length).toBe(1);
+
+			connection.close();
+
+			expect(peer.connections.length).toBe(0);
+			expect(spyClose).toHaveBeenCalled();
+		});
+
+		it('addConnection() - should execute pipeline on events', async () => {
+			const mockNotification = jest.fn();
+
+			await connection.emit('notification', mockNotification);
+			
+			expect(mockLoggerDebug).toHaveBeenCalledWith(DEBUG_MSG_ADD_CONNECTION);
+			expect(spyPipelineExecute).toHaveBeenCalledTimes(1);
+		});
+
+		it('addConnection() - notification event: should throw and catch when not handled by middleware', async () => {
+			peer.pipeline = pipelineWithoutMiddleware;
+
+			await connection.emit('notification');
+			expect(mockLoggerError).toHaveBeenCalledTimes(1);
+		});
+		
+		it('addConnection() - notification event: should not throw and catch when handled by middleware', async () => {
+			peer.pipeline = pipelineWithMiddleware;
+
+			await connection.emit('notification');
+			expect(mockLoggerError).not.toHaveBeenCalled();
+		});
+		
+		it('addConnection() - request event: should not call reject when handled by middleware', async () => {
+			peer.pipeline = pipelineWithMiddleware;
+
+			await connection.emit('request', mockRequest, mockRespond, mockReject);
+
+			expect(mockRespond).toHaveBeenCalledTimes(1);
+			expect(mockReject).not.toHaveBeenCalled();
+		});
+
+		it('addConnection() - request event: should call reject when not handled by middleware', async () => {
+			peer.pipeline = pipelineWithoutMiddleware;
+
+			await connection.emit('request', mockRequest, mockRespond, mockReject);
+
+			expect(mockReject).toHaveBeenNthCalledWith(1, REJECT_MESSAGE_SERVER_ERROR);
+		});
+
+		it('request() - should call request on connections', () => {
+			peer.pipeline = pipelineWithoutMiddleware;
+			peer.addConnection(connection);
+			
+			expect(spyRequest).not.toHaveBeenCalled();
+
+			peer.request(mockSocketMessage);
+
+			expect(spyRequest).toHaveBeenCalled();
+		});
+		
+		it('request() - should log warn on no connections', () => {
+			peer.pipeline = pipelineWithoutMiddleware;
+
+			peer.connections.remove(connection);
+			expect(peer.connections.length).toBe(0);
+			
+			peer.request(mockSocketMessage);
+
+			expect(mockLoggerWarn).toHaveBeenCalled();
 		});
 	});
 });
