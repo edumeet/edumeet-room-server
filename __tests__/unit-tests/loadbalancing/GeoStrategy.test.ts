60b7acf7f5f371fbe891fc234d346297f625b607
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-02-21 17:44:11 +0100

    test: add unit-tests for load balancing classes

--- __tests__/unit-tests/loadbalancing/GeoStrategy.test.ts
+++ __tests__/unit-tests/loadbalancing/GeoStrategy.test.ts
@@ -12,6 +12,7 @@ const mediaNodeMiddle = {
 	hostname: '194.177.32.0'
 } as unknown as MediaNode;
 const client = '5.44.192.0';
+const clientLocalhost = '127.0.0.1';
 
 test('Should not suggest remote media node', () => {
 	const stickyCandidates = [ mediaNodeRemote ];
@@ -38,4 +39,69 @@ test('Should use sticky node within threshold', () => {
 
 	expect(candidates[0]).toBe(mediaNodeClose);
 	expect(candidates.length).toBe(1);
+});
+
+test('Should use closest media-node on two active nodes within threshold', () => {
+	const stickyCandidates = [ mediaNodeMiddle, mediaNodeClose ];
+	const allNodes = [ mediaNodeRemote, mediaNodeClose, mediaNodeMiddle ];
+	const peer = { getAddress: () => { return client; } } as unknown as Peer;
+
+	const sut = new GeoStrategy();
+
+	const candidates = sut.getCandidates(allNodes, stickyCandidates, peer);
+
+	expect(candidates[0]).toBe(mediaNodeClose);
+	expect(candidates.length).toBe(2);
+});
+
+test('Should use closest media-node on no active nodes', () => {
+	const stickyCandidates: MediaNode[] = [];
+	const allNodes = [ mediaNodeRemote, mediaNodeClose, mediaNodeMiddle ];
+	const peer = { getAddress: () => { return client; } } as unknown as Peer;
+
+	const sut = new GeoStrategy();
+
+	const candidates = sut.getCandidates(allNodes, stickyCandidates, peer);
+
+	expect(candidates[0]).toBe(mediaNodeClose);
+	expect(candidates.length).toBe(3);
+});
+
+test('Should use media-node outside of threshold when no other available', () => {
+	const stickyCandidates: MediaNode[] = [];
+	const allNodes = [ mediaNodeRemote ];
+	const peer = { getAddress: () => { return client; } } as unknown as Peer;
+
+	const sut = new GeoStrategy();
+
+	const candidates = sut.getCandidates(allNodes, stickyCandidates, peer);
+
+	expect(candidates[0]).toBe(mediaNodeRemote);
+	expect(candidates.length).toBe(1);
+});
+
+test('Should keep sticky candidates when geoposition does not work', () => {
+	const stickyCandidates = [ mediaNodeRemote ];
+	const allNodes = [ mediaNodeClose, mediaNodeRemote ];
+	const peer = { getAddress: () => { return clientLocalhost; } } as unknown as Peer;
+
+	const sut = new GeoStrategy();
+
+	const candidates = sut.getCandidates(allNodes, stickyCandidates, peer);
+
+	expect(candidates[0]).toBe(mediaNodeRemote);
+	expect(candidates.length).toBe(1);
+});
+
+test('Should use first media-node when geoposition does not work', () => {
+	const stickyCandidates: MediaNode[] = [ ];
+	const allNodes = [ mediaNodeRemote, mediaNodeClose ];
+	const peer = { getAddress: () => { return clientLocalhost; } } as unknown as Peer;
+
+	const sut = new GeoStrategy();
+
+	const candidates = sut.getCandidates(allNodes, stickyCandidates, peer);
+
+	expect(candidates[0]).toBe(mediaNodeRemote);
+	expect(candidates.length).toBe(2);
 });
---
+++ __tests__/unit-tests/loadbalancing/StickyStrategy.test.ts
@@ -0,0 +1,65 @@
+import { StickyStrategy } from '../../../src/loadbalance/StickyStrategy';
+import MediaNode from '../../../src/media/MediaNode';
+import Room from '../../../src/Room';
+
+const mediaNode1 = { id: 'id1' } as unknown as MediaNode;
+const mediaNode2 = { id: 'id2' } as unknown as MediaNode;
+const mediaNode3 = { id: 'id3' } as unknown as MediaNode;
+
+test('Should only return active media-node', () => {
+	const activeRoomIds = new Set<string>();
+
+	activeRoomIds.add('id1');
+	const room = {
+		getActiveMediaNodes: jest.fn()
+	} as unknown as Room;
+	const spyGetActiveMediaNodes = jest.spyOn(room, 'getActiveMediaNodes').mockImplementation(() => {
+		return activeRoomIds;
+	});
+
+	const sut = new StickyStrategy();
+	const mediaNodes: MediaNode[] = [ mediaNode2, mediaNode1 ];
+
+	const candidates =	sut.getCandidates(mediaNodes, room);
+
+	expect(spyGetActiveMediaNodes).toHaveBeenCalled();
+	expect(candidates.length).toBe(1);
+});
+
+test('Should return all active media-node', () => {
+	const activeRoomIds = new Set<string>();
+
+	activeRoomIds.add('id3');
+	activeRoomIds.add('id2');
+	const room = {
+		getActiveMediaNodes: jest.fn()
+	} as unknown as Room;
+	const spyGetActiveMediaNodes = jest.spyOn(room, 'getActiveMediaNodes').mockImplementation(() => {
+		return activeRoomIds;
+	});
+
+	const sut = new StickyStrategy();
+	const mediaNodes: MediaNode[] = [ mediaNode1, mediaNode2, mediaNode3 ];
+
+	const candidates =	sut.getCandidates(mediaNodes, room);
+
+	expect(spyGetActiveMediaNodes).toHaveBeenCalled();
+	expect(candidates.length).toBe(2);
+});
+
+test('Should return empty array on no active media-node', () => {
+	const room = {
+		getActiveMediaNodes: jest.fn()
+	} as unknown as Room;
+	const spyGetActiveMediaNodes = jest.spyOn(room, 'getActiveMediaNodes').mockImplementation(() => {
+		return new Set<string>();
+	});
+
+	const sut = new StickyStrategy();
+	const mediaNodes: MediaNode[] = [ mediaNode2, mediaNode1 ];
+
+	const candidates =	sut.getCandidates(mediaNodes, room);
+
+	expect(spyGetActiveMediaNodes).toHaveBeenCalled();
+	expect(candidates.length).toBe(0);
+});
