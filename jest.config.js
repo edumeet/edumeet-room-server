263e3d2ed61b64ae9456903bbb23ba177943952d
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-02-22 09:37:42 +0100

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
-const { defaults: tsjPreset } = require('ts-jest/presets');
-
-/** @returns {Promise<import('jest').Config>} */
-
-module.exports = async () => {
-	return {
-		verbose: true,
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
 		modulePathIgnorePatterns: [ '<rootDir>/dist' ],
 		transform: {
-			...tsjPreset.transform,
-			'\\.[jt]s$': [
-				'ts-jest', { tsconfig: '<rootDir>/src/tsconfig.json' }
+			'^.+\\.[t]s$': [
+				'ts-jest', { tsconfig: 'src/tsconfig.json' }
 			]
 		}
-	};
-};
+	}
