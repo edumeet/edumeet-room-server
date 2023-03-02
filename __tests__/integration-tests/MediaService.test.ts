5d557eed6730434c0490c1296a519c01814e451c
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-03-02 17:48:11 +0100

    test: add integration-test for mediaservice

---
+++ __tests__/integration-tests/MediaService.test.ts
@@ -0,0 +1,71 @@
+import LBStrategyFactory from '../../src/loadbalancing/LBStrategyFactory';
+import LoadBalancer from '../../src/loadbalancing/LoadBalancer';
+import { Peer } from '../../src/Peer';
+import Room from '../../src/Room';
+import MediaService from '../../src/MediaService';
+import { Config } from '../../src/Config';
+
+/**
+ * Requires mediaNode running with config
+ * hostname: 127.0.0.1
+ * port: 3001
+ * secret: secret1
+ */
+
+test('getRouter() should throw on no mediaNodes', async () => {
+	const factory = new LBStrategyFactory([]);
+	const loadBalancer = new LoadBalancer(factory);
+	const config = {
+		mediaNodes: []
+	} as unknown as Config;
+	const sut = new MediaService({ loadBalancer, config });
+
+	const roomOptions = {
+		id: 'roomId',
+		mediaService: sut,
+	};
+	const room = new Room(roomOptions);
+	const peerOptions = {
+		id: 'peerId',
+		roomId: 'roomId'
+	};
+	const peer = new Peer(peerOptions);
+
+	await expect(sut.getRouter(room, peer)).rejects.toThrow();
+});
+
+test('getRouter() should get router', async () => {
+	const config = {
+		mediaNodes: [
+			{
+				hostname: '127.0.0.1',
+				port: 3001,
+				secret: 'secret1',
+				latitude: 55.676,
+				longitude: 12.568
+			}
+		]
+	} as unknown as Config;
+	const factory = new LBStrategyFactory([]);
+	const loadBalancer = new LoadBalancer(factory);
+	const sut = new MediaService({ loadBalancer, config });
+
+	const roomOptions = {
+		id: 'roomId',
+		mediaService: sut,
+	};
+	const room = new Room(roomOptions);
+	const peerOptions = {
+		id: 'peerId',
+		roomId: 'roomId'
+	};
+	const peer = new Peer(peerOptions);
+
+	expect(room.routers.length).toBe(0);
+
+	const router = await sut.getRouter(room, peer);
+
+	expect(room.routers.length).toBe(1);
+	expect(router.closed).toBeFalsy();
+	expect(router.appData.roomId).toBe(room.id);
+});
