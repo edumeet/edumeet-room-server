b2edae628645a078436d70c1c441dd1008bbcb86
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-03-29 20:26:04 +0200

    fix: log error messages in loadbalancer

--- src/LoadBalancer.ts
+++ src/LoadBalancer.ts
@@ -64,7 +64,7 @@ export default class LoadBalancer {
 			return candidates;
 
 		} catch (err) {
-			if (err instanceof Error) logger.error('Error while getting candidates');
+			logger.error(err);	
 			
 			return [];
 		}
