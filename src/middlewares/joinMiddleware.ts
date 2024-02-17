2f5b138362b7a31e039aa2eeca547b9b82972f7b
Håvar Aambø Fosstveit <havar@fosstveit.net>
2024-02-17 23:16:41 +0100

    Fix bug in PR merge

--- src/middlewares/joinMiddleware.ts
+++ src/middlewares/joinMiddleware.ts
@@ -27,15 +27,10 @@ export const createJoinMiddleware = ({ room }: { room: Room; }): Middleware<Peer
 				const {
 					displayName,
 					picture,
-					rtpCapabilities,
 				} = message.data;
-
-				if (!rtpCapabilities)
-					throw new Error('missing rtpCapabilities');
 
 				peer.displayName = displayName;
 				peer.picture = picture;
-				peer.rtpCapabilities = rtpCapabilities;
 
 				const lobbyPeers = peer.hasPermission(Permission.PROMOTE_PEER) ?
 					room.lobbyPeers.items.map((p) => (p.peerInfo)) : [];
