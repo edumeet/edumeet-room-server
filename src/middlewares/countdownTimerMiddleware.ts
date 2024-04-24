7d0ad59e5363b990fbd591e7e7e44690515edc55
Roman Drozd <roman@drozd.it>
2024-04-24 15:55:31 +0200

    Standarize names

--- src/middlewares/countdownTimerMiddleware.ts
+++ src/middlewares/countdownTimerMiddleware.ts
@@ -34,7 +34,7 @@ export const createCountdownTimerMiddleware = ({ room }: { room: Room }): Middle
 	
 				room.countdownTimer.isEnabled = true;
 	
-				room.notifyPeers('moderator:enableCountdownTimer', {
+				room.notifyPeers('moderator:enabledCountdownTimer', {
 					isEnabled: room.countdownTimer.isEnabled
 				}, peer);
 		
@@ -52,29 +52,12 @@ export const createCountdownTimerMiddleware = ({ room }: { room: Room }): Middle
 		
 				room.countdownTimer.isEnabled = false;
 		
-				room.notifyPeers('moderator:disableCountdownTimer', {
+				room.notifyPeers('moderator:disabledCountdownTimer', {
 					isEnabled: room.countdownTimer.isEnabled
 				}, peer);
 			
 				context.handled = true;				
 			
-				break;
-			}
-
-			case 'moderator:setCountdownTimer': {
-				// if (!hasPermission(room, peer, Permission.MODERATE_ROOM))
-				// 	throw new Error('peer not authorized');
-
-				const { left } = message.data;
-
-				room.countdownTimer.left = left;
-
-				room.notifyPeers('moderator:settedCountdownTimer', {
-					left: room.countdownTimer.left
-				});
-
-				context.handled = true;				
-
 				break;
 			}
 
@@ -85,12 +68,12 @@ export const createCountdownTimerMiddleware = ({ room }: { room: Room }): Middle
 				// if (!hasPermission(room, peer, Permission.MODERATE_ROOM))
 				// 	throw new Error('peer not authorized');
 				
-				room.countdownTimer.isRunning = true;
+				room.countdownTimer.isStarted = true;
 
-				// room.notifyPeers('moderator:settedCountdownTimer', {
+				// room.notifyPeers('moderator:setCountdownTimer', {
 				// 	// isEnabled: room.countdownTimer.isEnabled,
 				// 	// left: room.countdownTimer.left,
-				// 	isRunning: room.countdownTimer.isRunning
+				// 	isStarted: room.countdownTimer.isStarted
 				// });
 
 				clearInterval(room._countdownTimerRef);
@@ -108,29 +91,29 @@ export const createCountdownTimerMiddleware = ({ room }: { room: Room }): Middle
 					if (left === end || room.empty) {
 						clearInterval(room._countdownTimerRef);
 						
-						room.countdownTimer.isRunning = false;
+						room.countdownTimer.isStarted = false;
 						room.countdownTimer.left = '00:00:00';
 
-						// room.notifyPeers('moderator:settedCountdownTimer', {
+						// room.notifyPeers('moderator:setCountdownTimer', {
 						// 	left: room.countdownTimer.left,
-						// 	// isRunning: room.countdownTimer.isRunning
+						// 	// isStarted: room.countdownTimer.isStarted
 						// });
 
 						room.notifyPeers('moderator:stoppedCountdownTimer', {
-							isRunning: room.countdownTimer.isRunning
+							isStarted: room.countdownTimer.isStarted
 						});
 					}
 	
 				}, 1000);
 
-				room.notifyPeers('moderator:settedCountdownTimer', {
+				room.notifyPeers('moderator:setCountdownTimer', {
 					left: room.countdownTimer.left,
 				});
 
 				room.notifyPeers('moderator:startedCountdownTimer', {
 					// isEnabled: room.countdownTimer.isEnabled,
 					// left: room.countdownTimer.left,
-					isRunning: room.countdownTimer.isRunning
+					isStarted: room.countdownTimer.isStarted
 				});
 	
 				context.handled = true;				
@@ -142,22 +125,22 @@ export const createCountdownTimerMiddleware = ({ room }: { room: Room }): Middle
 			{
 				logger.debug('moderator:stopCountdownTimer ');
 	
-				if (room.countdownTimer.isRunning) {
+				if (room.countdownTimer.isStarted) {
 					// if (!this._hasPermission(peer, MODERATE_ROOM))
 					// 	throw new Error('peer not authorized');
 	
-					// room.countdownTimer.isRunning = false;
+					// room.countdownTimer.isStarted = false;
 	
 					clearInterval(room._countdownTimerRef);
-					room.countdownTimer.isRunning = false;
+					room.countdownTimer.isStarted = false;
 
-					room.notifyPeers('moderator:settedCountdownTimer', {
+					room.notifyPeers('moderator:setCountdownTimer', {
 						left: room.countdownTimer.left,
-						// isRunning: room.countdownTimer.isRunning
+						// isStarted: room.countdownTimer.isStarted
 					});
 
 					room.notifyPeers('moderator:stoppedCountdownTimer', {
-						isRunning: room.countdownTimer.isRunning
+						isStarted: room.countdownTimer.isStarted
 					});
 	
 				}
@@ -166,6 +149,24 @@ export const createCountdownTimerMiddleware = ({ room }: { room: Room }): Middle
 	
 				break;
 			}
+
+			case 'moderator:setCountdownTimer': {
+				// if (!hasPermission(room, peer, Permission.MODERATE_ROOM))
+				// 	throw new Error('peer not authorized');
+
+				const { left } = message.data;
+
+				room.countdownTimer.left = left;
+
+				room.notifyPeers('moderator:setCountdownTimer', {
+					left: room.countdownTimer.left
+				});
+
+				context.handled = true;				
+
+				break;
+			}
+			
 			default: {
 				break;
 			}
